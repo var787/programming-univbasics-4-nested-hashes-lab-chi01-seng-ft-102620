cec383c1a0561f2bb8ef28d87948d6dbfff1836e
Rakshan Hegde <hegde.rakshan@gmail.com>
2020-10-10 05:32:28 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,16 +84,14 @@ def adding_matz
         :languages => ["C"]
       }
     }
-    yuki={:yukihiro_matsumoto => {
-           :known_for => "Ruby",
-           :languages => ["LISP", "C"]
-    }
-  }
+  
+  
 
   programmer_hash[:yukihiro_matsumoto]={
     :known_for => "Ruby",
     :languages => ["LISP", "C"]
   }
+  programmer_hash
 
 end
 
